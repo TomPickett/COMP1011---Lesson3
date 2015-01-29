50338fa29f2be7f8702b8396dc6049aa66b83ed2
TomPickett <Pickett.tom7@gmail.com>
2015-01-28 23:24:00 -0500

    Crrated basic project

---
+++ src/Project.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tom Pickett
+ *Program Description: Demo Files for COMP1011 Lesson 3
+ *@version 0.1 - Created basic Project
+ */
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

536788d2c9e135ccb0c347b345ff7ada693edb42
TomPickett <Pickett.tom7@gmail.com>
2015-01-28 23:56:39 -0500

    Added Vehicle Class

--- src/Project.java
+++ src/Project.java
@@ -1,19 +1,17 @@
 /**
- * 
- */
-
-/**
  * @author Tom Pickett
  *Program Description: Demo Files for COMP1011 Lesson 3
- *@version 0.1 - Created basic Project
+ *@version 0.2 - Added Vehicle Class
  */
 public class Project {
 
 	/**
+	 * Main method for project
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Vehicle myVehicle = new Vehicle();
+		
 
 	}
 
